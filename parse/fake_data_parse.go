b524c70920bdfb6bb8f5c58805029de44f7fa39d
Vasil Trifonov <v.trifonov@gmail.com>
2016-11-11 19:15:48 +0200

    fixing regex replaces

--- parse/fake_data_parse.go
+++ parse/fake_data_parse.go
@@ -68,7 +68,7 @@ func (fdp FakeDataParse) ReplaceVars(req *definition.Request, input string) stri
 		} else if i := strings.Index(tag, "request.url."); i == 0 {
 			s, found = getStringPart(req.Path, tag[12:], "value")
 		} else if i := strings.Index(tag, "request.body."); i == 0 {
-			s, found = getStringPart(req.Body, tag[12:], "value")
+			s, found = getStringPart(req.Body, tag[13:], "value")
 		} else if i := strings.Index(tag, "request.query."); i == 0 {
 			s, found = getQueryStringParam(req, tag[14:])
 		} else if i := strings.Index(tag, "request.cookie."); i == 0 {
@@ -145,9 +145,12 @@ func getStringPart(input string, pattern string, groupName string) (string, bool
 
 	match := r.FindStringSubmatch(input)
 	result := make(map[string]string)
-	for i, name := range r.SubexpNames() {
-		if i != 0 {
-			result[name] = match[i]
+	names := r.SubexpNames()
+	if len(match) >= len(names) {
+		for i, name := range names {
+			if i != 0 {
+				result[name] = match[i]
+			}
 		}
 	}
 
