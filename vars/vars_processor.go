8bc62f16f4d86cfddabb7c7083cb63fc960798a0
Vasil Trifonov <v.trifonov@gmail.com>
2016-11-24 16:49:39 +0200

    fixed a problem with calculating persist vars needed in the actions + improved file persister

--- vars/vars_processor.go
+++ vars/vars_processor.go
@@ -13,16 +13,25 @@ type VarsProcessor struct {
 }
 
 func (fp VarsProcessor) Eval(req *definition.Request, m *definition.Mock) {
-	fp.walkAndFill(fp.FillerFactory.CreateRequestFiller(req), m)
-	fp.walkAndFill(fp.FillerFactory.CreateFakeFiller(fp.FakeAdapter), m)
+	requestFiller := fp.FillerFactory.CreateRequestFiller(req)
+	fakeFiller := fp.FillerFactory.CreateFakeFiller(fp.FakeAdapter)
+	persistFiller := fp.FillerFactory.CreatePersistFiller(fp.PersistEngines)
 	entityActions := persist.EntityActions{fp.PersistEngines}
+
+	fp.walkAndFill(requestFiller, m, true)
+	fp.walkAndFill(fakeFiller, m, true)
+
+	// we need to make sure the persisted vars are filled before executing the actions - as we need to make sure the persist vars are replaced in the persist actions
+	fp.walkAndFillPersisted(persistFiller, m)
+
 	entityActions.ApplyActions(m)
-	fp.walkAndFill(fp.FillerFactory.CreatePersistFiller(fp.PersistEngines), m)
+
+	fp.walkAndFill(persistFiller, m, false)
+
 }
 
-func (fp VarsProcessor) walkAndFill(f Filler, m *definition.Mock) {
+func (fp VarsProcessor) walkAndFill(f Filler, m *definition.Mock, fillPersisted bool) {
 	res := &m.Response
-	per := &m.Persist
 	amqp := &m.Notify.Amqp
 	for header, values := range res.Headers {
 		for i, value := range values {
@@ -34,12 +43,20 @@ func (fp VarsProcessor) walkAndFill(f Filler, m *definition.Mock) {
 		res.Cookies[cookie] = f.Fill(m, value, false)
 	}
 
-	res.Body = f.Fill(m, res.Body, false)
 	amqp.Body = f.Fill(m, amqp.Body, false)
+	res.Body = f.Fill(m, res.Body, false)
+
+	if fillPersisted {
+		fp.walkAndFillPersisted(f, m)
+	}
+}
+
+func (fp VarsProcessor) walkAndFillPersisted(f Filler, m *definition.Mock) {
+	per := &m.Persist
+
 	per.Entity = f.Fill(m, per.Entity, false)
 	per.Collection = f.Fill(m, per.Collection, true)
 	for action, value := range per.Actions {
 		per.Actions[action] = f.Fill(m, value, false)
 	}
-
 }
