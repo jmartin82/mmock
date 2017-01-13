fdca5095a3b9f9662ccc4f0d250055ed1cd4c9d8
Jordi Martin <jordimartin@gmail.com>
2017-01-13 12:39:30 +0100

    Enable path glob
    Enabled (again) path glob matching

--- match/mock_match_test.go
+++ match/mock_match_test.go
@@ -44,12 +44,26 @@ func TestMatchPath(t *testing.T) {
 	}
 }
 
-func TestGlobPath(t *testing.T) {
+func TestPathVars(t *testing.T) {
 	req := definition.Request{}
 	req.Path = "/a/b/c"
 
 	m := definition.Mock{}
 	m.Request.Path = "/a/:b/:c"
+
+	mm := MockMatch{}
+
+	if b, err := mm.Match(&req, &m); !b {
+		t.Error(err)
+	}
+}
+
+func TestPathGlob(t *testing.T) {
+	req := definition.Request{}
+	req.Path = "/a/b/c"
+
+	m := definition.Mock{}
+	m.Request.Path = "/a/*"
 
 	mm := MockMatch{}
 
