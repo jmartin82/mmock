499bf765488e159ab10cb6cfbbd21abd7b211383
Bartek Tofel <bartlomiej.tofel@teamcmp.com>
2017-09-29 18:41:31 +0200

    Adde the possibility to sorth mappings in the console in asc/desc order in 5 columns

---
+++ tmpl/js/sorting.js
@@ -0,0 +1,62 @@
+function sortTable(tableSelector, f, n){
+	var rows = $(tableSelector + ' tbody  tr').get();
+
+	rows.sort(function(a, b) {
+
+		var A = getVal(a);
+		var B = getVal(b);
+
+		if(A < B) {
+			return -1*f;
+		}
+		if(A > B) {
+			return 1*f;
+		}
+		return 0;
+	});
+
+	function getVal(elm){
+		var v = $(elm).children('td').eq(n).text().toUpperCase();
+		if($.isNumeric(v)){
+			v = parseInt(v,10);
+		}
+		return v;
+	}
+
+	$.each(rows, function(index, row) {
+		$(tableSelector).children('tbody').append(row);
+	});
+}
+
+var mappingElements = ['#uri', '#desc', '#method', '#path', '#result'];
+var mappingTableSelector = '#mapping-table';
+var selectorUp = '.glyphicon-chevron-up';
+var selectorDown = '.glyphicon-chevron-down';
+var selectedClassName = 'selected';
+
+function attachListener(item, index, elements){
+    $(item).find(selectorUp).on('click', function(){
+        var n = $(item).prevAll().length;
+        sortTable(mappingTableSelector, -1 ,n);
+        markAsSelected($(this));
+    });
+    $(item).find(selectorDown).on('click', function(){
+        var n = $(item).prevAll().length;
+        sortTable(mappingTableSelector, 1 ,n);
+        markAsSelected($(this));
+    });
+}
+
+function markAsSelected(object) {
+	clearSelectedClass();
+	object.addClass(selectedClassName);
+}
+
+function clearSelectedClass() {
+	$(selectorUp).removeClass(selectedClassName);
+	$(selectorDown).removeClass(selectedClassName);
+}
+
+function attachListeners() {
+    mappingElements.forEach(attachListener);
+}
