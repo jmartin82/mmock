19f7e4cd68438dee8c92b725db543adc96c12b7b
inaba jun <inabajun.for.regi@gmail.com>
2018-08-20 20:21:08 +0900

    create mapping via console (#63)

--- tmpl/js/mapping.js
+++ tmpl/js/mapping.js
@@ -21,6 +21,36 @@ function Mapping(domain) {
 
     this.fillList = fillList;
 
+    $('#mapping').on('click', '.btn-create-mapping', function() {
+        BootstrapDialog.show({
+            title: 'Mapping create',
+            message: $('<textarea id="text-create-mapping" style="min-width:570px;min-height:300px"></textarea>'),
+            buttons: [{
+                label: 'Cancel',
+                action: function(dialog) {
+                    dialog.close();
+                }
+            }, {
+                label: 'Save',
+                cssClass: 'btn-primary',
+                action: function(dialog) {
+                   var content = $('#text-create-mapping').val();
+                   var uri = JSON.parse(content).URI;
+                   var endpoint = window.location.protocol + "//" + domain + "/api/mapping/" + uri;
+                   $.ajax({
+                        type: 'POST',
+                        url: endpoint,
+                        data: content,
+                        success: function(data) {  dialog.close(); fillList() },
+                        error: function(data) { alert("Error: "+JSON.stringify(data)); dialog.close(); },
+                        contentType: "application/json",
+                        dataType: 'json'
+                    });
+                }
+            }]
+        });
+    });
+
     $('#mapping-data').on('click', '.btn-view-mapping', function() {
         var uri = $(this).data("uri");
         $.getJSON(window.location.protocol + "//" + domain + "/api/mapping/" + uri, function(data) {
