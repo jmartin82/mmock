5e8482fc6e126cc608e7a9ae2b5d9a4a266cd2a5
Santi Lertsumran <santi@odds.team>
2018-09-16 15:24:48 +0700

    [ADD] more unit test for getHeaderParam

--- vars/request_test.go
+++ vars/request_test.go
@@ -23,3 +23,33 @@ func TestGetHeaderParam(t *testing.T) {
 		t.Errorf("Couldn't get the content. Value: %s", v)
 	}
 }
+
+func TestGetHeaderParamNotFoundHeaderKey(t *testing.T) {
+	rp := Request{}
+	header := make(definition.Values)
+	header["Authorization"] = []string{"Bearer abc123"}
+	req := definition.Request{}
+	req.Headers = header
+	rp.Request = &req
+	_, f := rp.getHeaderParam("Authorization2")
+	if f {
+		t.Errorf("Header key found")
+	}
+}
+
+func TestGetHeaderParamWithOutHeaderValue(t *testing.T) {
+	rp := Request{}
+	header := make(definition.Values)
+	header["Authorization"] = []string{}
+	req := definition.Request{}
+	req.Headers = header
+	rp.Request = &req
+	v, f := rp.getHeaderParam("Authorization")
+	if f {
+		t.Errorf("Header key found")
+	}
+
+	if strings.EqualFold(v, "Bearer abc1235") {
+		t.Errorf("Couldn get the content. Value: %s", v)
+	}
+}
