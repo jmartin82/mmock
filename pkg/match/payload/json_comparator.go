dfc1cc8415454027412f75f40bc217972dfe68e7
joel-44 <49453069+joel-44@users.noreply.github.com>
2019-05-27 16:21:59 +0200

    Add support for comparing array of json objects (#87)
    * Add support for comparing array of json objects
    
    * Fix public function

--- pkg/match/payload/json_comparator.go
+++ pkg/match/payload/json_comparator.go
@@ -8,7 +8,34 @@ import (
 type JSONComparator struct {
 }
 
+func isArray(s1 string) bool {
+	return len(s1) > 0 && s1[0] == '['
+}
+
+func (jc *JSONComparator) compareArray(s1, s2 string) bool {
+
+	var o1 []interface{}
+	var o2 []interface{}
+
+	var err error
+	err = json.Unmarshal([]byte(s1), &o1)
+	if err != nil {
+		return false
+	}
+	err = json.Unmarshal([]byte(s2), &o2)
+	if err != nil {
+		return false
+	}
+
+	return reflect.DeepEqual(o1, o2)
+}
+
 func (jc *JSONComparator) Compare(s1, s2 string) bool {
+
+	if isArray(s1) || isArray(s2) {
+		return jc.compareArray(s1, s2)
+	}
+
 	var o1 interface{}
 	var o2 interface{}
 
