a93f9dd2bcfba1216ad171a941cc0ef421435eb3
Marco Reni <marco.reni@drivek.com>
2019-06-27 11:39:49 +0200

    Added tests

--- internal/vars/request_test.go
+++ internal/vars/request_test.go
@@ -2,6 +2,7 @@ package vars
 
 import (
 	"github.com/jmartin82/mmock/pkg/mock"
+	"os"
 	"strings"
 	"testing"
 )
@@ -50,5 +51,57 @@ func TestGetHeaderParamWithOutHeaderValue(t *testing.T) {
 
 	if strings.EqualFold(v, "Bearer abc1235") {
 		t.Errorf("Couldn get the content. Value: %s", v)
+	}
+}
+
+func TestGetEnvironmentVariable(t *testing.T) {
+	key := "env.TEST_VARIABLE"
+	value := "test value"
+
+	holders := []string{
+		key,
+	}
+
+	rp := Request{}
+
+	os.Setenv("TEST_VARIABLE", value)
+	vars := rp.Fill(holders)
+	os.Unsetenv("TEST_VARIABLE")
+
+	if len(vars) == 0 {
+		t.Errorf("Unable to retrieve vars")
+	}
+
+	v, ok := vars[key]
+
+	if !ok {
+		t.Errorf("Unable to retrieve value inside vars")
+	}
+
+	if len(v[0]) == 0 {
+		t.Errorf("Unable to retrieve value inside var element")
+	}
+
+	if !strings.EqualFold(v[0], value) {
+		t.Errorf("Couldn't get the expected value. Expected: %s, Value found: %s", value, v[0])
+	}
+}
+
+func TestGetEnvironmentVariableNotFound(t *testing.T) {
+	key := "env.TEST_VARIABLE_UNEXISTENT"
+	value := "test value"
+
+	holders := []string{
+		key,
+	}
+
+	rp := Request{}
+	
+	os.Setenv("TEST_VARIABLE", value)
+	vars := rp.Fill(holders)
+	os.Unsetenv("TEST_VARIABLE")
+
+	if len(vars) > 0 {
+		t.Errorf("Unexpectedly found variable")
 	}
 }
