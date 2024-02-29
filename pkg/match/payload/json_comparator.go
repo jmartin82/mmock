a586ccf3f31d927f28bc056c58c93a7b60651564
Roshan Patil <patilroshan443@gmail.com>
2024-02-29 19:30:55 +0100

    Support Regular Expressions for Field Values in JSON Request Body (#151)
    * Added support for the Regex in JSON body
    
    Signed-off-by: Roshan Patil <patilroshan443@gmail.com>
    
    * Updated README
    
    ---------
    
    Signed-off-by: Roshan Patil <patilroshan443@gmail.com>

--- pkg/match/payload/json_comparator.go
+++ pkg/match/payload/json_comparator.go
@@ -2,7 +2,9 @@ package payload
 
 import (
 	"encoding/json"
+	"fmt"
 	"reflect"
+	"regexp"
 	"strings"
 )
 
@@ -14,18 +16,51 @@ func isArray(s string) bool {
 	return len(st) > 0 && st[0] == '['
 }
 
-func (js *JSONComparator) doCompare(s1, s2 string, o1, o2 interface{}) bool {
-	var err error
-	err = json.Unmarshal([]byte(s1), &o1)
-	if err != nil {
+func (jc *JSONComparator) doCompareJSONRegex(jsonWithPatterns, jsonWithValues string) bool {
+	var patterns map[string]interface{}
+	var values map[string]interface{}
+	if err := json.Unmarshal([]byte(jsonWithPatterns), &patterns); err != nil {
 		return false
 	}
-	err = json.Unmarshal([]byte(s2), &o2)
-	if err != nil {
+	if err := json.Unmarshal([]byte(jsonWithValues), &values); err != nil {
 		return false
 	}
+	return match(patterns, values)
+}
 
-	return reflect.DeepEqual(o1, o2)
+func (jc *JSONComparator) doCompareArrayRegex(jsonWithPatterns, jsonWithValues string) bool {
+	var patterns []map[string]interface{}
+	var values []map[string]interface{}
+	if err := json.Unmarshal([]byte(jsonWithPatterns), &patterns); err != nil {
+		return false
+	}
+	if err := json.Unmarshal([]byte(jsonWithValues), &values); err != nil {
+		return false
+	}
+	for i := 0; i < len(patterns); i++ {
+		if !match(patterns[i], values[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func match(p, v map[string]interface{}) bool {
+	for field, pattern := range p {
+		value, exists := v[field]
+		if !exists {
+			return false
+		}
+		str, ok := pattern.(string)
+		if !ok {
+			return reflect.DeepEqual(pattern, value)
+		}
+		matched, err := regexp.MatchString(str, fmt.Sprint(value))
+		if err != nil || !matched {
+			return false
+		}
+	}
+	return true
 }
 
 func (jc *JSONComparator) Compare(s1, s2 string) bool {
@@ -35,13 +70,8 @@ func (jc *JSONComparator) Compare(s1, s2 string) bool {
 	}
 
 	if isArray(s1) || isArray(s2) {
-		var o1 []interface{}
-		var o2 []interface{}
-		return jc.doCompare(s1, s2, o1, o2)
+		return jc.doCompareArrayRegex(s1, s2)
 	}
 
-	var o1 interface{}
-	var o2 interface{}
-
-	return jc.doCompare(s1, s2, o1, o2)
+	return jc.doCompareJSONRegex(s1, s2)
 }
