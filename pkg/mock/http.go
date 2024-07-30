a698eed60637c498b432ac35e69663f5311a80d3
Roshan Patil <patilroshan443@gmail.com>
2024-07-30 11:46:01 +0200

    Added support for Server Side events
    Signed-off-by: Roshan Patil <patilroshan443@gmail.com>

--- pkg/mock/http.go
+++ pkg/mock/http.go
@@ -1,11 +1,14 @@
 package mock
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
+
+	"github.com/tidwall/gjson"
 )
 
 // HTTP is and adaptor beteewn the http and mock config.
@@ -39,7 +42,7 @@ func (t HTTP) BuildRequestDefinitionFromHTTP(req *http.Request) Request {
 		res.QueryStringParameters[name] = values
 	}
 
-	body, _ := ioutil.ReadAll(req.Body)
+	body, _ := io.ReadAll(req.Body)
 	res.Body = string(body)
 
 	return res
@@ -68,14 +71,34 @@ func getHostAndPort(req *http.Request) (string, string) {
 }
 
 // WriteHTTPResponseFromDefinition read a mock response and write a http response.
-func (t HTTP) WriteHTTPResponseFromDefinition(fr *Response, w http.ResponseWriter) {
+func (t HTTP) WriteHTTPResponseFromDefinition(fr *Response, w http.ResponseWriter, req *http.Request) {
+	if isSSE(fr) {
+		streamResponse(fr, w, req)
+		return
+	}
+	addHeadersAndCookies(fr, w)
+	w.WriteHeader(fr.StatusCode)
+	io.WriteString(w, fr.Body)
+}
 
+// Check if the response is of type SSE
+func isSSE(fr *Response) bool {
+	values, ok := fr.Headers["content-type"]
+	if ok {
+		for _, value := range values {
+			return strings.ToLower(value) == "text/event-stream"
+		}
+	}
+	return false
+}
+
+func addHeadersAndCookies(fr *Response, w http.ResponseWriter) {
 	for header, values := range fr.Headers {
 		for _, value := range values {
 			w.Header().Add(header, value)
 		}
-
 	}
+
 	if len(fr.Cookies) > 0 {
 		cookies := []string{}
 		for cookie, value := range fr.Cookies {
@@ -83,7 +106,21 @@ func (t HTTP) WriteHTTPResponseFromDefinition(fr *Response, w http.ResponseWrite
 		}
 		w.Header().Add("Set-Cookie", strings.Join(cookies, ";"))
 	}
+}
 
-	w.WriteHeader(fr.StatusCode)
-	io.WriteString(w, fr.Body)
+// streamResponse - stream response
+func streamResponse(fr *Response, w http.ResponseWriter, req *http.Request) {
+	addHeadersAndCookies(fr, w)
+
+	for _, response := range gjson.Parse(fr.Body).Array() {
+		time.Sleep(time.Second * 2)
+		select {
+		case <-req.Context().Done():
+			return
+		default:
+			ba, _ := json.Marshal((response.Value()))
+			fmt.Fprintf(w, "data: %s\n\n", string(ba))
+			w.(http.Flusher).Flush()
+		}
+	}
 }
