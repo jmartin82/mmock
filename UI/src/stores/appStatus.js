bf557812c6174019f9d1335635030279559d6495
Jordi Martin <jordimartin@gmail.com>
2025-06-11 23:18:02 +0200

    Improve UI

--- UI/src/stores/appStatus.js
+++ UI/src/stores/appStatus.js
@@ -2,16 +2,13 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useAppStatusStore = defineStore('appStatus', () => {
-  const isDark = ref(false)
   const isSidebarOpen = ref(false)
 
   function toggleSidebar(){
     isSidebarOpen.value = !isSidebarOpen.value
   }
   
-  function toggleDarkMode(){
-    isDark.value = !isDark.value
-  }
+ 
 
-  return { isDark, isSidebarOpen, toggleSidebar, toggleDarkMode }
+  return { isSidebarOpen, toggleSidebar }
 })
